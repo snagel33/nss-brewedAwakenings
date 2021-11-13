05478de1f983a7eaa9e4b5f8ec6ef23ccfc89389
Steve Nagel <stevencnagel@gmail.com>
2021-11-13 13:17:21 -0600

    added click event on products

--- scripts/Products.js
+++ scripts/Products.js
@@ -1,13 +1,29 @@
 import { getProducts } from "./database.js"
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("product")) {
+            const [,productId] = itemClicked.id.split("--")
+
+            for (const product of products) {
+                if (product.id === parseInt(productId)) {
+                    window.alert(`${product.name} costs $${product.price}`)
+                }
+            }
+        }
+    }
+)
+
 const products = getProducts()
 
 export const Products = () => {
     let html = "<ul>"
 
     for (const product of products) {
-        // html += `<li id="product--${product.id}">${product.name}</li>`
-        html += `<li>${product.name}</li>`
+        html += `<li id="product--${product.id}">${product.name}</li>`
+        // html += `<li>${product.name}</li>`
     }
 
     html += "</ul>"
