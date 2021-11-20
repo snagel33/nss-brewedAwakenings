33e2ddd15121981fd66bb4234fc99743f5acabeb
Steve Nagel <stevencnagel@gmail.com>
2021-11-20 13:16:51 -0600

    final commit

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,6 +1,30 @@
-import { getEmployees } from "./database.js"
+import { getEmployees, getOrders } from "./database.js"
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("employee")) {
+            const [,employeeId] = itemClicked.id.split("--")
+            let numberOrders = 0;
+            let currentEmployee = "";
+            for (const employee of employees) {
+
+                if (employee.id === parseInt(employeeId)) {
+                    currentEmployee = employee.name
+                    numberOrders = orders.filter(
+                        order => (order.employeeId === parseInt(employeeId))
+                        ).length
+                }
+            }
+                window.alert(`${currentEmployee} sold ${numberOrders} products`)
+                
+        }
+    }
+)
 
 const employees = getEmployees()
+const orders = getOrders()
 
 export const Employees = () => {
     let html = "<ul>"
--- scripts/Products.js
+++ scripts/Products.js
@@ -5,8 +5,7 @@ document.addEventListener(
     (clickEvent) => {
         const itemClicked = clickEvent.target
         if (itemClicked.id.startsWith("product")) {
-            const [,productId] = itemClicked.id.split("--")
-
+            const [,productId] = itemClicked.id.split("--")        
             for (const product of products) {
                 if (product.id === parseInt(productId)) {
                     window.alert(`${product.name} costs $${product.price}`)
